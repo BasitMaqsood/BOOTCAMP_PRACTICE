d6a72f4f981e3c45d1f5b5699fa5be3d76e4e1a0
Basit <malikbasitmaqsood@gmail.com>
2020-12-28 05:25:05 +0500

    fetch datat via fetch api

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
-import React, { useState } from 'react';
-
-import Parent from './components/Parent';
-
-import CounterContext from './CounterContext';
-
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [repos, setRepos] = useState([]);
+  useEffect(() => {
 
-  const countState = useState(1);
+    async function fetchRepos() {
+      const response = await fetch('https://api.github.com/users/BasitMaqsood/repos');
+      const data = await response.json();
+      setRepos(data);
+    }
+
+    fetchRepos();
+
+  }, [])
+
 
   return (
-    <CounterContext.Provider value={countState}>
-      <div className="App">
-        <Parent />
-      </div>
-    </CounterContext.Provider>
+    <div className="App">
+      <h4>API CALLING</h4>
+      <ul>
+        {repos.map(repo => {
+          return (
+            <li style={{ listStyleType: 'none' }} key={repo.id}>{repo.name}</li>
+          )
+        })}
+      </ul>
+    </div>
   );
 }
 
